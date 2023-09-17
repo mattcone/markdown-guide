ed49b70411c60927db0513893c371062e8e3805c
Matt Cone <mattcone@users.noreply.github.com>
2023-09-17 17:56:55 -0600

    Update build command

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-go get -u github.com/tdewolff/minify
+brew install tdewolff/tap/minify
 
 minify -r -b -o assets/css/homebundle.css assets/css/bootstrap-toc.min.css assets/css/bootstrap.min.css assets/css/docsearch.min.css assets/css/homepage.css
 minify -r -b -o assets/css/pagebundle.css assets/css/bootstrap-toc.min.css assets/css/bootstrap.min.css assets/css/docsearch.min.css assets/css/page.css
